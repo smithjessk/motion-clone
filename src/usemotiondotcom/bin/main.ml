a8d527b75507d31173fa1f7d28d9bc8b9486df6f
Jess Smith <smith.jessk@gmail.com>
2023-02-21 12:57:49 -0500

    check in work

--- src/usemotiondotcom/bin/main.ml
+++ src/usemotiondotcom/bin/main.ml
@@ -1,1 +1,18 @@
-let () = print_endline "Hello, World!"
+open Core
+
+let () = 
+
+print_endline "Hello World";;
+
+
+(* TODO set up ocamlformat *)
+
+open Async
+let web_document_trawler () =
+  let open Deferred.Let_syntax in
+  let%bind () = Async_js.init () in
+  let%bind () = Async_js.sleep 1.0 in
+  let%bind () = Async_js.sleep 1.0 in
+  let%bind () = Async_js.sleep 1.0 in
+  let%bind () = Async_js.sleep 1.0 in
+  let%bind () = Async_js.sleep 1.0 in
